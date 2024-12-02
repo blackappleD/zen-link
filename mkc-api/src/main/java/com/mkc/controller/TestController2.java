48d5d54b6b0cd11d515414f95573b52ff951b917
chent <aChen1874@163.com>
2024-12-02 14:37:54 +0800

    fix: 修改超时时间

--- mkc-api/src/main/java/com/mkc/controller/TestController2.java
+++ mkc-api/src/main/java/com/mkc/controller/TestController2.java
@@ -27,7 +27,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -62,7 +61,6 @@ public class TestController2 {
 	/**
 	 * 全国高等学历信息查询 /educationInfo 日志分析
 	 */
-	@PostConstruct
 	public void educationInfo() {
 
 		Map<String, Statistic> map = new HashMap<>();
--- mkc-api/src/main/java/com/mkc/tool/SignUtils.java
+++ mkc-api/src/main/java/com/mkc/tool/SignUtils.java
@@ -20,13 +20,16 @@ public class SignUtils {
 	public static void main(String[] args) {
 
 		String persons = "[{\"name\":\"倪春云\",\"cardNum\":\"321323198606074120\"}]";
-		String merCode = "XZSZKJSON1";
+		String merCode = "LXZL";
 		//		本地
 //		String pwd = "e0be01493778d77ecfd2004f54b41a09";
 		//线上
-		String pwd = "dc86ec7091b13263d44a7f23ab868385";
+		String pwd = "23d0418220cb3ddbf108b440bcab3560";
 
-		String plaintext = merCode + "魏佳宁"+ "RNg8m3kIRezwtqVfujGixYdbn9l2Q5AyFckJMkZbVSc=";
+		//(merCode+idCard+
+		// name+mobile+pwd
+
+		String plaintext = merCode + "330381199910181122" + "杨鑫" + "16604322562";
 
 		System.out.println(sign(plaintext, pwd));
 	}
