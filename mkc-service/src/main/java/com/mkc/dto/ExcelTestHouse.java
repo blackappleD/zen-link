dde928e6d010fabdd995d31953e9bcd0c838f24d
blackappled <978767524@qq.com>
2025-03-04 13:57:33 +0800

    fix：不动产跑数结果查询接口excel导出字段修改

--- mkc-service/src/main/java/com/mkc/dto/ExcelTestHouse.java
+++ mkc-service/src/main/java/com/mkc/dto/ExcelTestHouse.java
@@ -16,42 +16,42 @@ public class ExcelTestHouse {
     @ExcelProperty("身份证号")
     private String personCardNum;
     @ColumnWidth(25)
-    @ExcelProperty("code")
+    @ExcelProperty("状态码")
     private String code;
     @ColumnWidth(25)
-    @ExcelProperty("reqOrderNo")
+    @ExcelProperty("订单号")
     private String reqOrderNo;
     @ColumnWidth(25)
-    @ExcelProperty("certNo")
+    @ExcelProperty("不动产权证号")
     private String certNo;
     @ColumnWidth(25)
-    @ExcelProperty("unitNo")
+    @ExcelProperty("不动产单元号")
     private String unitNo;
     @ColumnWidth(25)
-    @ExcelProperty("location")
+    @ExcelProperty("坐落")
     private String location;
     @ColumnWidth(25)
-    @ExcelProperty("ownership")
+    @ExcelProperty("共有情况")
     private String ownership;
     @ColumnWidth(25)
-    @ExcelProperty("houseArea")
+    @ExcelProperty("房屋面积")
     private String houseArea;
     @ColumnWidth(25)
-    @ExcelProperty("rightsType")
+    @ExcelProperty("权利类型")
     private String rightsType;
     @ColumnWidth(25)
-    @ExcelProperty("isSealUp")
+    @ExcelProperty("是否查封")
     private String isSealUp;
     @ColumnWidth(25)
-    @ExcelProperty("isMortgaged")
+    @ExcelProperty("是否抵押")
     private String isMortgaged;
     @ColumnWidth(25)
-    @ExcelProperty("rightsStartTime")
+    @ExcelProperty("使用期限起始时间")
     private String rightsStartTime;
     @ColumnWidth(25)
-    @ExcelProperty("rightsEndTime")
+    @ExcelProperty("使用期限结束时间")
     private String rightsEndTime;
     @ColumnWidth(25)
-    @ExcelProperty("useTo")
+    @ExcelProperty("用途")
     private String useTo;
 }
